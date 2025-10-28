aa659e6c1056277833f909122e06b55b9cab8c87
Leysha Antenor <159083391+Leyley0323@users.noreply.github.com>
2025-10-28 16:18:20 -0400

    Refactor PageTitle component with logo and new title

--- frontend/src/components/PageTitle.tsx
+++ frontend/src/components/PageTitle.tsx
@@ -1,8 +1,16 @@
-function PageTitle()
-{
-   return(
-     <h1 id="title">COP 4331 MERN Stack Demo</h1>
-   );
-};
+import React from "react";
+import logo from "../assets/cart.svg";
+
+function PageTitle() {
+  return (
+    <div id="page-title" style={{ textAlign: "center" }}>
+      {/* SVG image */}
+      <img src={logo} alt="Cheap Cart Logo" width="100" height="100" />
+
+      {/* Title text */}
+      <h1 id="title">Welcome to Cheap Cart!</h1>
+    </div>
+  );
+}
 
 export default PageTitle;
