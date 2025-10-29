97808eb3756491105ddd2ca08cf5054d77bf8df6
Evan Wilcher <ev666507@ucf.edu>
2025-10-28 23:40:17 -0400

    Updated routes including adding HomePage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,16 @@
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
 
-import LoginPage from './pages/LoginPage';
+import HomePage from './pages/HomePage';
+import Login from './components/Login.tsx';
 import CardPage from './pages/CardPage';
 
 function App() {
   return (
-    <Router >
+    <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<HomePage />} />   {/* default homepage */}
+        <Route path="/Login.tsx" element={<Login />} />
         <Route path="/cards" element={<CardPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>  
