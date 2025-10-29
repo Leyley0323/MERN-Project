13e6c5cb2a9e416b311eacec2cad0ec8cb6b0406
Leysha Antenor <159083391+Leyley0323@users.noreply.github.com>
2025-10-29 02:34:37 -0400

    Add Login and SignUp pages to routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,9 @@ import './App.css';
 import HomePage from './pages/HomePage';
 import Login from './components/Login.tsx';
 import CardPage from './pages/CardPage';
+import LoginPage from './pages/LoginPage';
+import SignUp from './components/SignUp.tsx';
+import SignUpPage from './pages/SignUpPage';
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
         <Route path="/Login.tsx" element={<Login />} />
         <Route path="/cards" element={<CardPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/SignUp.tsx" element={<SignUp />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>  
     </Router>
   );
