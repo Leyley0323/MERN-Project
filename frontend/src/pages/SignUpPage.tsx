01dd43ceba9e66abc114c60d719419e18ff58346
Leysha Antenor <159083391+Leyley0323@users.noreply.github.com>
2025-10-29 02:35:34 -0400

    Add files via upload

---
+++ frontend/src/pages/SignUpPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function SignUp() {
+        return (
+        <div>
+        <h2>Create Account</h2>
+        <input type="text" placeholder="Email" /><br />
+        <input type="text" placeholder="Username" /><br />
+        <input type="password" placeholder="Password" /><br />
+        <button>Create Account</button>
+        </div>
+    );
+}
+
+export default SignUp;
